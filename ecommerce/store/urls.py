28922d5cc4d6f2be4b0351e0c5b18368767dc1cb
KhatriSuson <susonkhatri110@gmail.com>
2024-10-16 22:27:26 +0545

    update add-to-cart urls

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('product/<int:id>/', views.product_detail, name='product_detail'),
     path('cart/', views.cart, name='cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+
 ]
