038ad1facddf5584de3f238b96b7c533da314b5c
KhatriSuson <susonkhatri110@gmail.com>
2024-10-16 22:20:40 +0545

    create views

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, ShoppingCart
 
-# Create your views here.
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+def cart(request):
+    return render(request, 'store/cart.html')
