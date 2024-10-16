14858166b8d18f2055e8eb4258a6a462c377e54a
KhatriSuson <susonkhatri110@gmail.com>
2024-10-16 23:35:40 +0545

    update add_to-cart function

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -5,28 +5,21 @@ from .models import Product, Order, OrderItem
 from django.contrib.auth.decorators import login_required
 
 def add_to_cart(request, product_id):
-    # Get the product the user is adding to the cart
     product = Product.objects.get(id=product_id)
 
-    # Check if the user is authenticated
     if request.user.is_authenticated:
-        # If the user is authenticated, use their customer account
         customer = request.user.customer
-        # Get or create an order for the user
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+        order_item.quantity += 1
+        order_item.save()
     else:
-        # If the user is not authenticated, create a temporary cart using session data
-        if 'order' not in request.session:
-            request.session['order'] = {}
-        
-        # Retrieve the session order (temporary cart)
-        order = request.session['order']
-    
-    # Add the product to the order (or cart)
-    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+        # Handle anonymous user with session
+        cart = request.session.get('cart', {})
+        if product_id in cart:
+            cart[product_id] += 1
+        else:
+            cart[product_id] = 1
+        request.session['cart'] = cart
 
-    # You can set the quantity or other details here
-    order_item.quantity += 1
-    order_item.save()
-
-    return redirect('cart')  # Redirect to cart page
+    return redirect('cart')
