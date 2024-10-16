3a0d871c2c6efcafb2733fc8fe46a94d9cc82c61
KhatriSuson <susonkhatri110@gmail.com>
2024-10-16 22:14:34 +0545

    update model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,28 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Customer(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    address = models.TextField()
 class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.user.username
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    product = models.ManyToManyField(Product)
     order_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=100)
+    complete = models.BooleanField(default=False)
 
 class ShoppingCart(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ManyToManyField(Product)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField(default=1)
